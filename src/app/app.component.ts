df5328ddb3cc9566a8ce337a12c227a39ff33d6e
CNN87 <125652955+CNN87@users.noreply.github.com>
2023-04-13 11:01:24 +0200

    modul12 video10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'sinansims.de';
+export class AppComponent implements OnInit {
+  images = ['codebg.jpg', 'laptop.jpg', 'beuth.jpg'];
+  currentImage = 0;
+  showImage = true;
 
-  // javascript content
+  
+  ngOnInit(){
+    this.updateImage()
+
+  }
+
+  updateImage() {
+    setInterval( () => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() =>{
+        this.showImage = true;
+      }, 10);
+
+    }, 8000);
+  }
+  
 }
